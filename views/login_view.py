fb62b72465089c64364bb343c285a12fec9a3804
Shihab Al Jahwari <alshohb@gmail.com>
2024-04-24 19:49:02 -0400

    Create login view interface

---
+++ views/login_view.py
@@ -0,0 +1,16 @@
+from getpass import getpass
+
+def show_login_form():
+    """Display the login form to the user and get their input."""
+    print("Please log in to continue:")
+    username = input("Username: ")
+    password = getpass("Password: ") 
+    return username, password
+
+def show_login_success(user_id):
+    """Display a success message upon login."""
+    print(f"Login successful. Welcome, user {user_id}!")
+
+def show_login_failure():
+    """Inform the user that the login has failed."""
+    print("Login failed. Please check your credentials and try again.")
